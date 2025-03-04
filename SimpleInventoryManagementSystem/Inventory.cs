30bab3421335480ca37d462b7b90e5c0e13487c7
Tariq Omar <tariqomar2004@gmail.com>
2025-03-05 01:19:38 +0200

    Finish Modifications

--- SimpleInventoryManagementSystem/Inventory.cs
+++ SimpleInventoryManagementSystem/Inventory.cs
@@ -1,37 +1,33 @@
-﻿
-using System;
+﻿using System;
 using SimpleInventoryManagementSystem;
 
-
 namespace SimpleInventoryManagementSystem;
+
 public class Inventory
 {
     private List<Product> _products;
-    private const string _currency="$";
-    private InputLogger _input;
+    private const string _currency = "$";
+    private readonly InputLogger _input = new InputLogger();
 
     public Inventory(ICollection<Product> products)
     {
         _products = (List<Product>) products;
-        _input = new InputLogger();
     }
 
     public  void AddProduct()
     {
-        Product product=_input.ProductInput();
+        Product product = _input.ProductInput();
         _products.Add(product);
     }
 
     public void ViewProducts()
     {
         Console.WriteLine("The menu of products : ");
-        foreach (Product product in _products)
+        foreach (var product in _products)
         {
             Console.WriteLine($"Name is : {product._name}, Price is: {product._price} {_currency}, Quantity is: {product._quantity}");
         }
-
         Console.WriteLine("==========================");
-
     }
 
 }
