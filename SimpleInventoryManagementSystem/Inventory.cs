a6d46b637e867f4182257697dc3519de94cb0ac5
Tariq Omar <tariqomar2004@gmail.com>
2025-03-13 14:44:19 +0200

    Bye

--- SimpleInventoryManagementSystem/Inventory.cs
+++ SimpleInventoryManagementSystem/Inventory.cs
@@ -1,26 +1,27 @@
-﻿using SimpleInventoryManagementSystem;
+﻿using System;
+using SimpleInventoryManagementSystem;
 
 public class Inventory
 {
-    private List<Product> products;
+    private List<Product> _products;
     private const string Currency = "$";
-    private readonly InputLogger input = new InputLogger();
+    private readonly InputLogger _inputLogger = new InputLogger();
 
     public Inventory(ICollection<Product> products)
     {
-        this.products = products.ToList();
+        this._products = products.ToList();
     }
 
     public void AddProduct()
     {
-        Product product = input.ProductInput();
-        products.Add(product);
+        Product product = _inputLogger.ProductInput();
+        _products.Add(product);
     }
 
     public void ViewProducts()
     {
         Console.WriteLine("The menu of products:");
-        foreach (var product in products)
+        foreach (var product in _products)
         {
             Console.WriteLine($"Name: {product.Name}, Price: {product.Price} {Currency}, Quantity: {product.Quantity}");
         }
